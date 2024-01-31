a578250745a8a6ec637aaf47d38b8d9d3617081c
Hamed <ostovari.eng@gmail.com>
2024-01-31 16:26:32 +0400

    added register route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,8 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import mongoose from 'mongoose';
+import userRoutes from './routes/users';
 mongoose
   .connect(
     (process.env.MONGODB_CONNECTION_STRING as string).replace(
@@ -11,7 +12,8 @@ mongoose
   )
   .then(() => {
     console.log('DB connected');
-  });
+  })
+  .catch(() => console.log('is not connected'));
 
 const app = express();
 
@@ -19,11 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get('/api/test', (req: Request, res: Response) => {
-  res.status(200).json({
-    message: 'OK',
-  });
-});
+app.use('/api/users', userRoutes);
 
 app.listen(7000, () => {
   console.log('app running');
